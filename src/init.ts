9248b2fbfd809d90cb0bdd4b8e9ca370b124e1d1
Kevin Sullivan <kevin.sullivan@axolotl.industries>
2018-08-28 22:04:20 -0500

    Refactor attribute creation into a function

--- src/init.ts
+++ src/init.ts
@@ -1,5 +1,6 @@
 import { fragment, vertex } from './shaders'
 import { createProgram, createShader, resizeCanvas } from './util'
+import { createAttribute } from './util/createAttribute'
 
 /**
  * Initialize WebGL canvas
@@ -19,41 +20,27 @@ function init(el: HTMLCanvasElement) {
   const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragment)
   const program = createProgram(gl, vertexShader, fragmentShader)
 
-  const colorUniformLocation = gl.getUniformLocation(program, 'u_color')
-  const positionAttributeLocation = gl.getAttribLocation(program, 'a_position')
-
-  const positionBuffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-
-  const positions = [0, 0, 0, 0.5, 0.7, 0]
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW)
+  gl.useProgram(program)
 
   const va = gl.createVertexArray()
-  gl.bindVertexArray(va)
-  gl.enableVertexAttribArray(positionAttributeLocation)
+  if (va === null) {
+    throw new Error('Cannot create vertex array')
+  }
 
-  const size = 2
-  const type = gl.FLOAT
-  const normalize = false
-  const stride = 0
-  const pointerOffset = 0
-  gl.vertexAttribPointer(
-    positionAttributeLocation,
-    size,
-    type,
-    normalize,
-    stride,
-    pointerOffset,
-  )
+  const positions = new Float32Array([0, 0, 0, 0.5, 0.7, 0])
+  createAttribute('a_position', positions, gl, program, va, gl.STATIC_DRAW, {
+    size: 2,
+    type: gl.FLOAT,
+  })
+
+  const colorUniformLocation = gl.getUniformLocation(program, 'u_color')
+  gl.uniform4f(colorUniformLocation, 1, 0, 0.5, 1)
+
+  gl.bindVertexArray(va)
 
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
   gl.clearColor(0, 0, 0, 0)
   gl.clear(gl.COLOR_BUFFER_BIT)
-
-  gl.useProgram(program)
-
-  gl.uniform4f(colorUniformLocation, 1, 0, 0.5, 1)
-  gl.bindVertexArray(va)
 
   const primitiveType = gl.TRIANGLES
   const drawOffset = 0
---
+++ src/util/createAttribute.ts
@@ -0,0 +1,85 @@
+/**
+ * Create shader attribute
+ *
+ * @param gl WebGL2 rendering context
+ * @param program shader program
+ * @param vertexArray vertex array object
+ * @param name name of attribute (variable name in shader source)
+ * @param data typed attribute data
+ * @param usage usage hint, such as `STATIC_DRAW`
+ */
+export function createAttribute(
+  name: string,
+  data: AttributeData,
+  gl: WebGL2RenderingContext,
+  program: WebGLProgram,
+  vertexArray: WebGLVertexArrayObject,
+  usage: AttributeUsage,
+  layout: AttributeLayout,
+) {
+  // Load attribute data into buffer
+  const buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+  gl.bufferData(gl.ARRAY_BUFFER, data, usage)
+
+  // Enable the attribute with bound buffer
+  const location = gl.getAttribLocation(program, name)
+  gl.bindVertexArray(vertexArray)
+  gl.enableVertexAttribArray(location)
+
+  // Configure attribute data layout
+  const { size, type, normalize = false, stride = 0, offset = 0 } = layout
+  gl.vertexAttribPointer(location, size, type, normalize, stride, offset)
+}
+
+type AttributeData =
+  | number
+  | Int8Array
+  | Int16Array
+  | Int32Array
+  | Uint8Array
+  | Uint16Array
+  | Uint32Array
+  | Uint8ClampedArray
+  | Float32Array
+  | Float64Array
+  | DataView
+  | ArrayBuffer
+
+type AttributeUsage = number
+
+interface AttributeLayout {
+  /**
+   * Components per vertex
+   */
+  size: 1 | 2 | 3 | 4
+
+  /**
+   * Data type, such as:
+   *
+   * - `gl.BYTE`
+   * - `gl.SHORT`
+   * - `gl.UNSIGNED_BYTE`
+   * - `gl.UNSIGNED_BYTE`
+   * - `gl.UNSIGNED_SHORT`
+   * - `gl.FLOAT`
+   * - `gl.HALF_FLOAT`
+   */
+  type: number
+
+  /**
+   * Normalize non-floats into `[-1, 1]` or `[0, 1]` for
+   * unsigned types.
+   */
+  normalize?: boolean
+
+  /**
+   * Width in bytes between each vertex
+   */
+  stride?: number
+
+  /**
+   * Bytes before the first vertex
+   */
+  offset?: number
+}
