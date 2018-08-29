3b88ab0bb6433661b08e8f4846c58303567554cb
Kevin Sullivan <kevin.sullivan@axolotl.industries>
2018-08-28 22:08:36 -0500

    Comments for rendering setup

--- src/init.ts
+++ src/init.ts
@@ -11,34 +11,42 @@ import { createAttribute } from './util/createAttribute'
  * @param el HTML canvas DOM element
  */
 function init(el: HTMLCanvasElement) {
+  // Get rendering context
   const gl = el.getContext('webgl2')
   if (gl === null) {
     throw new Error('Cannot get WebGL 2 context')
   }
 
+  // Compile shader program
   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertex)
   const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragment)
   const program = createProgram(gl, vertexShader, fragmentShader)
 
   gl.useProgram(program)
 
+  // Setup vertex array for attributes
   const va = gl.createVertexArray()
   if (va === null) {
     throw new Error('Cannot create vertex array')
   }
 
+  // Create attributes
   const positions = new Float32Array([0, 0, 0, 0.5, 0.7, 0])
   createAttribute('a_position', positions, gl, program, va, gl.STATIC_DRAW, {
     size: 2,
     type: gl.FLOAT,
   })
 
+  // Create uniforms
   const colorUniformLocation = gl.getUniformLocation(program, 'u_color')
   gl.uniform4f(colorUniformLocation, 1, 0, 0.5, 1)
 
   gl.bindVertexArray(va)
 
+  // Update viewport
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+
+  // Render data
   gl.clearColor(0, 0, 0, 0)
   gl.clear(gl.COLOR_BUFFER_BIT)
 
